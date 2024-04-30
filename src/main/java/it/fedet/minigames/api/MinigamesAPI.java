50b37686e09ab5df3c6bc7c8dded299ddc9c6a32
xFedeT <tagliani.fede@gmail.com>
2024-04-30 11:02:31 +0200

    Initial Commit

---
+++ src/main/java/it/fedet/minigames/api/MinigamesAPI.java
@@ -0,0 +1,17 @@
+package it.fedet.minigames.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinigamesAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
