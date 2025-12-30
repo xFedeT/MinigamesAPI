a561f48b1bf958ffc3ebc56d875ed58fa02d9b0e
Federico Tagliani <tagliani.fede@gmail.com>
2025-12-30 17:25:28 +0100

    api: completed new structure of minigmaes
    Took 5 minutes

--- sumo/src/main/java/it/fedet/minigames/impl/sumo/game/team/SpectatorTeam.java
+++
@@ -1,9 +0,0 @@
-package it.fedet.minigames.impl.sumo.game.team;
-
-public class SpectatorTeam extends SumoTeam {
-
-    protected SpectatorTeam(int id, int gameID) {
-        super(id, gameID);
-    }
-
-}
